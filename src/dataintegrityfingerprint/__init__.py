3c7c8cd5756c6db823815dfade95a9d236db00dd
Florian Krause <siebenhundertzehn@gmail.com>
2021-12-12 18:03:32 +0100

    Updated version to 0.7.2

--- src/dataintegrityfingerprint/__init__.py
+++ src/dataintegrityfingerprint/__init__.py
@@ -9,7 +9,7 @@ For specification see https://github.com/expyriment/DIF.
 __author__ = 'Oliver Lindemann <oliver@expyriment.org>, ' \
              'Florian Krause <florian@expyriment.org>'
 
-__version__ = '0.7.1'
+__version__ = '0.7.2'
 
 
 from .dif import DataIntegrityFingerprint
